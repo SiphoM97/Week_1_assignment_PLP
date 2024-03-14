eb0843ac899e336344127f420360ed3f5e572c65
siphosetu mzayiya <siphosetumzayiya@siphosetus-MacBook-Air.local>
2024-03-15 01:01:57 +0200

    Initial commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+variable = input("Whats your name")
+age = input("how old are you ?")
+location = input("where are you located ?")
+
+print("Hello " + variable + " you are " + age + ", you also live in " + location)
